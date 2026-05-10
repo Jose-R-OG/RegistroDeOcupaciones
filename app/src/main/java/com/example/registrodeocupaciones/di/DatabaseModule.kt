1b9eadbef79b18db23317058204c3517c5871889
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-05-10 19:13:27 -0400

    Terminando la capa de datos

---
+++ app/src/main/java/com/example/registrodeocupaciones/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.registrodeocupaciones.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.registrodeocupaciones.data.local.OcupacionDao
+import com.example.registrodeocupaciones.data.local.OcupacionDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): OcupacionDb {
+        return Room.databaseBuilder(
+            context,
+            OcupacionDb::class.java,
+            "Ocupaciones.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideOcupacionDao(db: OcupacionDb): OcupacionDao {
+        return db.ocupacionDao()
+    }
+}

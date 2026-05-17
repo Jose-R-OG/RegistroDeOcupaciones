31fe1c6b6a27beb4483dd930c696a335cad66b97
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-05-17 17:40:46 -0400

    Refactor: Modificacion de MainActivity y apartado de navegacion para agregar NavigationBar

--- app/src/main/java/com/example/registrodeocupaciones/MainActivity.kt
+++ app/src/main/java/com/example/registrodeocupaciones/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.registrodeocupaciones.presentation.navigation.MineNavHost
 import dagger.hilt.android.AndroidEntryPoint
-import com.example.registrodeocupaciones.presentation.navigation.OcupacionNavHost
+import com.example.registrodeocupaciones.presentation.navigation.Screen
 import com.example.registrodeocupaciones.ui.theme.RegistroDeOcupacionesTheme
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -24,14 +34,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RegistroDeOcupacionesTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                val navController = rememberNavController()
+                val items = listOf(
+                    TopLevelRoute("Empleado", Screen.EmpleadoList, Icons.Default.Person),
+                    TopLevelRoute("Ocupacion", Screen.OcupacionList, Icons.Default.Home)
+                )
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            val navBackStackEntry by navController.currentBackStackEntryAsState()
+                            val currentDestination = navBackStackEntry?.destination
 
-                    OcupacionNavHost()
+                            items.forEach { item ->
+                                NavigationBarItem(
+                                    icon = { Icon(item.icono, contentDescription = item.nombre) },
+                                    label = { Text(item.nombre) },
+                                    selected = currentDestination?.hierarchy?.any {
+                                        it.hasRoute(item.ruta::class)
+                                    } == true,
+                                    onClick = {
+                                        navController.navigate(item.ruta) {
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    MineNavHost(
+                        navController = navController,
+                        innerPadding = innerPadding
+                    )
                 }
             }
         }
     }
 }
+
+data class TopLevelRoute<T : Any>(
+    val nombre: String,
+    val ruta: T,
+    val icono: androidx.compose.ui.graphics.vector.ImageVector
+)
